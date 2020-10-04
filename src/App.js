b54924ca34b83d6c724da059e1092101a1a3277b
Christine <christinesunnjm@gmail.com>
2020-10-04 14:20:32 -0400

    implemented form input

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
 
+// import components
+import Form from './components/Form';
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1>multilife</h1>
-        
-        
       </header>
-      <body>
         <h2>Ever feel like you're living a double life? Triple life? We get you. </h2>
         <h3>Efficiently manage all of your tasks in one place so you can stay on top of the game as the boss that you are. ;D</h3> 
-      </body>
+      <Form />
     </div>
   );
 }
